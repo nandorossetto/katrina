fc9ba37bb5988d9dcee90401944fcd9e41eefdb4
Fernando Rossetto <nandorossetto@gmail.com>
2015-02-10 21:27:33 -0200

    Fix test

--- test/controllersSpec.js
+++ test/controllersSpec.js
@@ -1,20 +1,23 @@
 describe('#1 Unit: ListController', function(){
-    var scope, httpBackend, controller;
+    var scope, httpBackend;
 
     beforeEach(module('ListingModule'));
 
     // Initialize the controller and a mock scope
     beforeEach(angular.mock.inject(function($httpBackend, $rootScope, $controller) {
         httpBackend = $httpBackend;
-        httpBackend.expectGET('http://www.mocky.io/v2/54da65f4267da33b0bb0f396').respond(200);
-
         scope = $rootScope.$new();
-        controller = $controller('ListController', { $scope: scope }); 
+        
+        $controller('ListController', { $scope: scope }); 
+        httpBackend.expectGET('http://www.mocky.io/v2/54da65f4267da33b0bb0f396').respond([
+            {"client_1": ''},
+            {"client_2": ''}
+        ]);
         
     }));
 
     it('should load JSON with items', function(){
-        expect(scope.clients);
         httpBackend.flush();
+        expect(scope.clients.length).toBe(2);
     });
 });
