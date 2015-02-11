1139b8d85d4c82faa199760c8fae3482c998e229
Fernando Rossetto <nandorossetto@gmail.com>
2015-02-11 09:24:45 -0200

    fix table format

--- app/static/js/controllers/list-controller.js
+++ app/static/js/controllers/list-controller.js
@@ -6,7 +6,15 @@ ListController.controller('ListController', ['$scope', '$http',
             method: 'GET',
             url: 'http://www.mocky.io/v2/54da65f4267da33b0bb0f396'
         }).success(function(data){
+            var clientItems = [];
+
+            for(var i in data){
+                clientItems = data[i].order;
+            }
+
             $scope.clients = data;
+            $scope.items = clientItems;
+
 
         }).error(function(){
             console.log('Não carregou o JSON');
